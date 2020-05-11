cd780241240e320e01148af079e2ea33fa4d6359
ajay <ajay.yadav5929@gmail.com>
2020-05-11 19:49:10 +0530

    First Commit
    Uploading files

---
+++ devops_assignment/src/devops_assignment/ques3.java
@@ -0,0 +1,10 @@
+package devops_assignment;
+import java.util.*;
+public class ques3 {
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("My name is Ajay Yadav");
+	}
+
+}
